a6889225486cc32d32c4788b287301c5609854f7
Виктор Виноградов <idalys1357@gmail.com>
2023-07-11 22:08:54 +0300

    multer

--- index.ts
+++ index.ts
@@ -7,6 +7,8 @@ import {GetMeController, LoginController, RegistrationController} from './contro
 import {ErrorValidator} from './validators/errorValidator.ts';
 import {CheckAuth} from './validators/checkAuth.ts';
 import {CreatePost, GetPosts} from './controllers/PostsController.ts';
+import multer from 'multer'
+import path from 'path';
 
 dotenv.config();
 const app = express();
@@ -25,6 +27,25 @@ mongoose.connection.on('connected', () => console.log('Connected to MongoDB'));
 
 app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads')
+  },
+  filename(_, file: Express.Multer.File, callback: (error: (Error | null), filename: string) => void) {
+    file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8')
+    callback(null, file.originalname);
+  }
+})
+
+const upload = multer({storage});
+
+app.post('/upload',CheckAuth, upload.single('image'), ((req, res) => {
+  res.status(200).json({
+    url: `uploads/${req.file?.originalname}`
+  })
+}))
 
 app.post('/auth/login', LoginValidator, ErrorValidator, LoginController);
 app.post('/auth/register', ExpressValidators, ErrorValidator, RegistrationController);
